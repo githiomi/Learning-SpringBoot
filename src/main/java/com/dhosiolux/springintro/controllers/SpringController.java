18a0f4b83e19098fdc0e081d5035f2c3b193cca1
Daniel Githiomi <danielgithiomi@gmail.com>
2023-07-01 14:55:51 +0400

    Can successfully return a list of user objects

--- src/main/java/com/dhosiolux/springintro/controllers/SpringController.java
+++ src/main/java/com/dhosiolux/springintro/controllers/SpringController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @RestController
 public class SpringController {
@@ -26,6 +28,25 @@ public class SpringController {
                 LocalDate.of(2001, 8, 27),
                 Gender.MALE
         );
+    }
+
+    @GetMapping(path = "/users")
+    protected ArrayList<User> getUsers(){
+        return new ArrayList<User>(
+                Arrays.asList(
+                        new User(
+                                "Daniel Githiomi",
+                                "daniel@githiomi.com",
+                                LocalDate.of(2001, 8, 27),
+                                Gender.MALE
+                        ),
+                        new User(
+                                "Faith Wamaitha",
+                                "faith@wamaitha.com",
+                                LocalDate.of(1998, 7, 17),
+                                Gender.FEMALE
+                        )
+        ));
     }
 
     @GetMapping(path = "/hello")
